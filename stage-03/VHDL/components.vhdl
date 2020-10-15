8331209b96c1f2db0b13ce99af7f68c80fa4ff34
Juanma Rico <juanmard@gmail.com>
2020-10-15 10:28:58 +0200

    vhdl: changed pongGame.vhdl

--- stage-03/VHDL/components.vhdl
+++ stage-03/VHDL/components.vhdl
@@ -3,6 +3,79 @@ context ieee.ieee_std_context;
 
 package components is
 
+    --// Draw a tenis court.
+    component court
+        port (
+            px_clk   : in  std_logic;
+            strVGA   : in  std_logic_vector (22 downto 0);
+            strRGB   : out std_logic_vector (25 downto 0)
+        );
+    end component;
+
+    --// Draw scoreboard.
+    component scoreboard
+        port (
+            px_clk    : in  std_logic;
+            strRGB_i  : in  std_logic_vector (25 downto 0);
+            dyn_clk   : in  std_logic;
+            reset     : in  std_logic;
+            goal_ply1 : in  std_logic;
+            goal_ply2 : in  std_logic;
+            strRGB_o  : out std_logic_vector (25 downto 0)
+        );
+    end component;
+
+    --// Draw players.
+    component verticalPlayers
+        port (
+            px_clk    : in  std_logic;
+            strRGB_i  : in  std_logic_vector (25 downto 0);
+            pos_ply1  : in  std_logic_vector (9 downto 0);
+            pos_ply2  : in  std_logic_vector (9 downto 0);
+            strRGB_o  : out std_logic_vector (25 downto 0)
+        );
+    end component;
+
+    --// Draw ball.
+    component ball
+        port (
+            px_clk    : in  std_logic;
+            strRGB_i  : in  std_logic_vector (25 downto 0);
+            pos_x     : in  std_logic_vector (9 downto 0);
+            pos_y     : in  std_logic_vector (9 downto 0);
+            strRGB_o  : out std_logic_vector (25 downto 0)
+        );
+    end component;
+
+    --// Sound card module.
+    component soundCard
+        port (
+            snd_clk : in std_logic;
+            channel : in std_logic_vector (1 downto 0);
+            sound   : in std_logic_vector (1 downto 0);
+            right_o : out std_logic;
+            left_o  : out std_logic
+        );
+    end component;
+
+    --// Dynamic's game.
+    component dynamicGame
+        port (
+            dyn_clk     : in std_logic;
+            reset       : in  std_logic;
+            play        : in  std_logic;
+            pos_ply1    : in  std_logic_vector (9 downto 0);
+            pos_ply2    : in  std_logic_vector (9 downto 0);
+            reset_goals : out std_logic;
+            goal_ply1   : out std_logic;
+            goal_ply2   : out std_logic;
+            x_ball      : out std_logic_vector (9 downto 0);
+            y_ball      : out std_logic_vector (9 downto 0);
+            channel     : out std_logic_vector (1 downto 0);
+            sound       : out std_logic_vector (1 downto 0)
+        );
+    end component;
+
     --// Control game module.
     component ctlButtons
         port (
--- stage-03/VHDL/pongGame.vhdl
+++ stage-03/VHDL/pongGame.vhdl
@@ -1,7 +1,7 @@
 library ieee;
 context ieee.ieee_std_context;
 
-use work.components.pongGame;
+use work.components.all;
 use work.streams.all;
 
 entity pongGame_vhdl is
@@ -21,8 +21,17 @@ end;
 
 architecture arch of pongGame_vhdl is
 
-  signal strVGA_stdv : std_logic_vector (22 downto 0);  -- Stream VGA.
-  signal strRGB_stdv : std_logic_vector (25 downto 0);  -- Stream RGB.
+  signal strVGA_stdv : std_logic_vector (22 downto 0);                                 --// Stream VGA.
+  signal strRGB_p0, strRGB_p1, strRGB_p2, strRGB_p3 : std_logic_vector (25 downto 0);  --// Local streams RGB.
+  
+  signal endframe    : std_logic;
+  signal reset_goals : std_logic;
+  signal goal_ply1   : std_logic;
+  signal goal_ply2   : std_logic;
+  signal channel     : std_logic_vector (1 downto 0);
+  signal sound       : std_logic_vector (1 downto 0);
+  signal x_ball      : std_logic_vector (9 downto 0);
+  signal y_ball      : std_logic_vector (9 downto 0);
 
 begin
 
@@ -32,27 +41,87 @@ begin
   strVGA_stdv(1)            <= strVGA.vsync;
   strVGA_stdv(0)            <= strVGA.active;
 
-  pongGame_0: pongGame
+  --// Draw a tenis court.
+  court_0: court
   port map (
-    px_clk   => px_clk,
-    strVGA   => strVGA_stdv,
-    reset    => reset,
-    play     => play,
-    snd_clk  => snd_clk,
-    pos_ply1 => pos_ply1,
-    pos_ply2 => pos_ply2,
-    strRGB   => strRGB_stdv,
-    right    => right,
-    left     => left
+      px_clk => px_clk,
+      strVGA => strVGA_stdv,
+      strRGB => strRGB_p0
   );
 
-  strRGB.R             <= strRGB_stdv(25);
-  strRGB.G             <= strRGB_stdv(24);
-  strRGB.B             <= strRGB_stdv(23);
-  strRGB.strVGA.x      <= unsigned(strRGB_stdv(22 downto 13));
-  strRGB.strVGA.y      <= unsigned(strRGB_stdv(12 downto 3));
-  strRGB.strVGA.hsync  <= strRGB_stdv(2);
-  strRGB.strVGA.vsync  <= strRGB_stdv(1);
-  strRGB.strVGA.active <= strRGB_stdv(0);
+  --// Draw scoreboard.
+  scoreboard_0: scoreboard
+  port map (
+      px_clk    => px_clk,
+      strRGB_i  => strRGB_p0,
+      dyn_clk   => endframe,
+      reset     => reset_goals,
+      goal_ply1 => goal_ply1,
+      goal_ply2 => goal_ply2,
+      strRGB_o  => strRGB_p1
+  );
+
+  --// Draw players.
+  verticalPlayers_0: verticalPlayers
+  port map (
+      px_clk   => px_clk,
+      strRGB_i => strRGB_p1,
+      pos_ply1 => pos_ply1,
+      pos_ply2 => pos_ply2,
+      strRGB_o => strRGB_p2
+  );
+
+  --// Draw ball.
+  ball_0: ball
+  port map (
+      px_clk   => px_clk,
+      strRGB_i => strRGB_p2,
+      pos_x    => x_ball,
+      pos_y    => y_ball,
+      strRGB_o => strRGB_p3
+  );
+
+  strRGB.R             <= strRGB_p3(25);
+  strRGB.G             <= strRGB_p3(24);
+  strRGB.B             <= strRGB_p3(23);
+  strRGB.strVGA.x      <= unsigned(strRGB_p3(22 downto 13));
+  strRGB.strVGA.y      <= unsigned(strRGB_p3(12 downto 3));
+  strRGB.strVGA.hsync  <= strRGB_p3(2);
+  strRGB.strVGA.vsync  <= strRGB_p3(1);
+  strRGB.strVGA.active <= strRGB_p3(0);
+
+  --// Sound card module.
+  soundCard_0: soundCard
+  port map (
+      snd_clk => snd_clk,
+      channel => channel,
+      sound   => sound,
+      right_o => right,
+      left_o  => left
+  );
+
+  --// Get endframe signal for dynamic logic.
+  endframeVGA_0: entity work.endframeVGA 
+  port map (
+      strVGA   => strVGA,
+      endframe => endframe
+  );
+
+  --// Change dynamic's game every frame.
+  dynamicGame_0: dynamicGame
+  port map (
+      dyn_clk     => endframe,
+      reset       => reset,
+      play        => play,
+      pos_ply1    => pos_ply1,
+      pos_ply2    => pos_ply2,
+      reset_goals => reset_goals,
+      goal_ply1   => goal_ply1,
+      goal_ply2   => goal_ply2,
+      x_ball      => x_ball,
+      y_ball      => y_ball,
+      channel     => channel,
+      sound       => sound
+  );
 
 end;
