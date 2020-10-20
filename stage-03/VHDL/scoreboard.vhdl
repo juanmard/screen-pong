1c04c7d4348177abf7f37b99eb2304fc298a53ae
Juanma Rico <juanmard@gmail.com>
2020-10-20 02:26:23 +0200

    vhdl: New scoreboard.vhdl, with only 2321 LUT.

--- stage-03/VHDL/scoreboard.vhdl
+++ stage-03/VHDL/scoreboard.vhdl
@@ -1,7 +1,7 @@
 --////////////////////////////////////////////////////////////////////////////////
 --// Company:     Ridotech
 --// Engineer:    Juan Manuel Rico
---// Create Date: 15/10/2020
+--// Create Date: 20/10/2020
 --// Module Name: scoreboard.vhdl
 --//
 --// Description: Draw a scoreboard in a stream RGB.
@@ -9,7 +9,7 @@
 --// Dependencies: 
 --//
 --// Revisions: 
---//     0.01 - File created.
+--//     0.02 - File created.
 --//
 --// Additional Comments:
 --//
@@ -41,11 +41,10 @@ architecture scoreboard_A of scoreboard is
     constant offset        : integer := 20;
 
     -- // Segments dimnentions.
-    constant wseg : integer := 30;   -- // Width of a horizontal segment.
-    constant hseg : integer := 10;   -- // Height of a horizontal segment.
-    constant wblock : integer := wseg;
-    constant hblock : integer := 2*wseg - hseg;
-
+    constant wseg   : integer := 30;             -- // Width of a horizontal segment.
+    constant hseg   : integer := 10;             -- // Height of a horizontal segment.
+    constant wblock : integer := wseg;           -- // Width of a digit block.
+    constant hblock : integer := 2*wseg - hseg;  -- // Height of a digit block.
 
     --// Calculate digits positions.
     constant pos_xply1 : integer := width_screen/2 - width_counter - offset;
@@ -62,21 +61,22 @@ architecture scoreboard_A of scoreboard is
 
     -- // Array blocks. Definition and initialization.
     type property_block_t is record
-        x     : integer;                        -- // Position x of the block.
-        y     : integer;                        -- // Position y of the block.
+        x     : integer;                        -- // Position x of the block (left-up corner).
+        y     : integer;                        -- // Position y of the block (left-up corner).
         num   : integer;                        -- // Number in the block.
         color : std_logic_vector (2 downto 0);  -- // Color of the number.
     end record;
     type blocks_t is Array (0 to 3) of property_block_t;
 
-    --constant test : property_block_t := (x => 100, y => 100, num => 3, color=>"110");
+    -- // Data blocks of digits.
     signal data : blocks_t := (
-        0 => (x => pos_xply1,                           y => pos_y, num => 0, color => white),
-        1 => (x => pos_xply1 + width_digit + separator, y => pos_y, num => 1, color => white),
-        2 => (x => pos_xply2,                           y => pos_y, num => 2, color => white),
-        3 => (x => pos_xply2 + width_digit + separator, y => pos_y, num => 3, color => white)
+        0 => (x => pos_xply1,                           y => pos_y, num => 0, color => white),  -- // Goals player 1 decens.
+        1 => (x => pos_xply1 + width_digit + separator, y => pos_y, num => 1, color => white),  -- // Goals player 1 units.
+        2 => (x => pos_xply2,                           y => pos_y, num => 2, color => white),  -- // Goals player 2 decens.
+        3 => (x => pos_xply2 + width_digit + separator, y => pos_y, num => 3, color => white)   -- // Goals player 2 units.
         );
 
+    -- // Test for numbers.
     type test_t is Array (0 to 3) of integer;
     signal nums : test_t;
 
@@ -86,9 +86,10 @@ architecture scoreboard_A of scoreboard is
     signal score_1u, score_1d, score_1u_q, score_1d_q : integer;
     signal score_2u, score_2d, score_2u_q, score_2d_q : integer;
 
-    --// Goal signals.
-    signal counter : integer := 10;          -- // Counter 1/70 Hz *10 ~ 0.15s
-    signal goal_1, goal_2 : std_logic;
+    -- // Goals signals to filter.
+    signal counter, counter_q : integer;
+    signal goal_1, goal_1_q : std_logic;
+    signal goal_2, goal_2_q : std_logic;
 
 begin
     -- // Extract actual pixel.
@@ -116,82 +117,72 @@ begin
              3 when x_px > data(3).x and x_px < data(3).x + wblock and
                     y_px > data(3).y and y_px < data(3).y + hblock else 1;
 
-    --// Get digit block data.
-    x_digit <= data(blk).x;
-    y_digit <= data(blk).y;
-    -- num_digit <= data(blk).num;
-    num_digit <= nums(blk);
-
+--// Another posibility...
 -- look_for:   for ind in 0 to 3 generate
 --                     blk(ind) <= '1' when
 --                             x_px > data(ind).x and x_px < data(ind).x + wseg   and
 --                             y_px > data(ind).y and y_px < data(ind).y + 2*wseg else '0';
 --             end generate;
 
+    -- // Get digit block data.
+    x_digit <= data(blk).x;
+    y_digit <= data(blk).y;
+    -- num_digit <= data(blk).num; -- (Don't work).
+    num_digit <= nums(blk);
+
     --// Draw digits.
     counter_ply1 : entity work.digit 
     generic map (
-        -- // TODO: Convert in a port.
-        color => green  -- data(blk).color
-        ----------------------
+        color => green  -- data(blk).color -- (Need to pass like port)
         )
     port map (
         px_clk   => px_clk,
         strRGB_i => strRGB_i,
-        --// TODO: Group in a record.
         pos_x    => x_digit,
         pos_y    => y_digit,
         num      => num_digit,
-        ----------------------
         strRGB_o => strRGB_o
     );
 
-
-    -- // Test. Not final code. 
-    score_1u_q <= score_1u + 1 when counter = 20 and goal_1 = '1' else
+    -- // Update the score.
+    score_1u_q <= score_1u + 1 when counter = 0 and goal_1 = '1' else
                              0 when score_1u = 10  else score_1u;
-    score_1d_q <= score_1d + 1 when score_1u = 10  else score_1d;
+    score_1d_q <= score_1d + 1 when score_1u = 10  else
+                             0 when score_1d = 10  else score_1d;
 
-
-    score_2u_q <= score_2u + 1 when counter = 20 and goal_2 = '1' else
+    score_2u_q <= score_2u + 1 when counter = 0 and goal_2 = '1' else
                              0 when score_2u = 10  else score_2u;
-    score_2d_q <= score_2d + 1 when score_2u = 10  else score_2d;
+    score_2d_q <= score_2d + 1 when score_2u = 10  else
+                             0 when score_2d = 10  else score_2d;
 
-    -- // Update goals numbers.
-    process (px_clk, reset)
+    -- // Filter goals signals.
+    counter_q <= 1000 when goal_ply1 = '1' or goal_ply2 = '1' else counter;
+    goal_1_q <= '1' when (goal_ply1 = '1' or goal_1 = '1') and counter > 0 else '0'; 
+    goal_2_q <= '1' when (goal_ply2 = '1' or goal_2 = '1') and counter > 0 else '0'; 
+
+    -- // Update memory and reset.
+    process (px_clk)
     begin
         if rising_edge(px_clk) then
-
             if reset then
-                score_1u <= 0;
                 score_1d <= 0;
-                score_2u <= 0;
+                score_1u <= 0;
                 score_2d <= 0;
+                score_2u <= 0;
             else
                 blk <= blk_q;
-                score_1u <= score_1u_q;
-                score_1d <= score_1d_q;
-                score_2u <= score_2u_q;
-                score_2d <= score_2d_q;
-            end if;
-
-            --// Detect goals and filter signals.
-            if goal_ply1 = '1' then
-                counter <= 1000;
-                goal_1 <= '1';
-            elsif goal_ply2 = '1' then
-                counter <= 1000;
-                goal_2 <= '1';
-            else
                 if counter > 0 then
-                    counter <= counter - 1;
+                    counter <= counter_q - 1;
                 else
-                    counter <= 0;
-                    goal_1 <= '0';
-                    goal_2 <= '0';
+                    counter <= counter_q;
                 end if;
+                goal_1 <= goal_1_q;
+                goal_2 <= goal_2_q;
+                score_1d <= score_1d_q;
+                score_1u <= score_1u_q;
+                score_2d <= score_2d_q;
+                score_2u <= score_2u_q;
             end if;
-
         end if;
     end process;
 
